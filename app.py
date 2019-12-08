8e8df037b1196b5ad665ad2e071dbef3c84f8048
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2019-12-08 14:51:28 -0800

    Users can see comments

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
+comments = db.comments
 
 
 
@@ -56,7 +57,10 @@ def playlists_submit():
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
+    # Add the below line:
+    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
+    # Edit the return statement to be the following:
+    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
 
     # ---------------------------------------------
@@ -91,6 +95,26 @@ def playlists_delete(playlist_id):
     """Delete one playlist."""
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
+
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+
+    comment = {
+        'title' : request.form.get("title"),
+        'content' : request.form.get("content"),
+        'playlist_id': ObjectId(request.form.get("playlist_id"))
+    }
+    print(comment)
+
+    comment_id = comments.insert_one(comment).inserted_id
+
+    """Submit a new comment."""
+      # TODO: Fill in the code here to build the comment object,
+    # and then insert it into the MongoDB comments collection
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+
+
 
 
 if __name__ == '__main__':
